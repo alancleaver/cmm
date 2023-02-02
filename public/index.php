189b7d064e3a91b81bb25f9d67e37bc0bc52ac29
Alan Cleaver <alan.cleaver@squaresystems.co.uk>
2023-02-02 19:29:27 +0000

    Added request to router load

--- public/index.php
+++ public/index.php
@@ -3,4 +3,4 @@
 require __DIR__ . '/../bootstrap/init.php';
 
 $router = new App\Router();
-$router->load($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+$router->load($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD'], $_REQUEST);
